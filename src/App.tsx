6fdc5aa1c4fa7a90a8f777741e5d7fcbca0a2121
Pravee2003 <137625770+Pravee2003@users.noreply.github.com>
2025-12-13 03:55:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const App: React.FC = () => {
             style={{
               position: "absolute",
               left: 400,
-              bottom: 70,
+              bottom: -60,
               width: 150,
               zIndex: 50,
               pointerEvents: "none",
